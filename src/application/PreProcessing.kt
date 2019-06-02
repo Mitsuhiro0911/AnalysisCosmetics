dd2eeb31c91670d1a8546e5c95b70fecb2c059d4
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-06-02 14:07:46 +0900

    成分リスト(重複排除前)の変数名を変更

--- src/application/PreProcessing.kt
+++ src/application/PreProcessing.kt
@@ -6,15 +6,15 @@ class PreProcessing{
     /**
      * 同義語辞書を用いて、表記揺れを解消する
      *
-     * @param [list] 同義語の統一処理が行われる前のリスト
+     * @param [componentList] 同義語の統一処理が行われる前のリスト
      * @return [unifiedList] 同義語の統一処理が行われたリスト
      */
-    fun unitySynonym(list: MutableList<String>): MutableList<String> {
+    fun unitySynonym(componentList: MutableList<String>): MutableList<String> {
 
         val unifiedList: MutableList<String> = mutableListOf()
-        for (i in 0..list.size - 1) {
+        for (i in 0..componentList.size - 1) {
             val test: List<Node> =
-                Constants.cosmeComponentDictionary.selectNodes("//component[text()='" + list.get(i) + "']/ancestor-or-self::*/representation//component")
+                Constants.cosmeComponentDictionary.selectNodes("//component[text()='" + componentList.get(i) + "']/ancestor-or-self::*/representation//component")
                 unifiedList.add(test.get(0).text)
         }
         return unifiedList
